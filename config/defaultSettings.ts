2e6251a8f275552fa1ba9aa49f332ee1457099f0
Cheney Veron <cheneyveron@live.cn>
2022-02-09 22:14:21 +0800

    feat(build): Add npm build:gh-page

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -4,18 +4,17 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  "navTheme": "light",
-  "layout": "top",
-  "contentWidth": "Fixed",
-  "fixedHeader": false,
-  "fixSiderbar": true,
-  "pwa": false,
-  "logo": "/logo-trans.png",
-  "headerHeight": 60,
-  "splitMenus": false,
-  "footerRender": false,
-  "menuRender": false
+  navTheme: 'light',
+  layout: 'top',
+  contentWidth: 'Fixed',
+  fixedHeader: false,
+  fixSiderbar: true,
+  pwa: false,
+  logo: './logo-trans.png',
+  headerHeight: 60,
+  splitMenus: false,
+  footerRender: false,
+  menuRender: false,
 };
 
 export default Settings;
-
--- src/components/PPContainer/index.tsx
+++ src/components/PPContainer/index.tsx
@@ -4,6 +4,13 @@ import styles from './index.less';
 export type SiderTheme = 'light' | 'dark';
 
 const PPContainer: React.FC = (props) => {
-  return <div className={`${styles.container}`}>{props.children}</div>;
+  return (
+    <div
+      className={`${styles.container}`}
+      style={{ backgroundImage: 'url(./pics/background.png)' }}
+    >
+      {props.children}
+    </div>
+  );
 };
 export default PPContainer;
