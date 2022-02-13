9c60d03feb2d015d889aec7840a66158457947dd
Youssef Harby <youssef_harby@yahoo.com>
2022-02-13 11:42:03 +0200

    locale(ar): add ar-EG and translate some menus

---
+++ src/locales/ar-EG/component.ts
@@ -0,0 +1,8 @@
+export default {
+  'component.tagSelect.expand': 'فتح',
+  'component.tagSelect.collapse': 'غلق ',
+  'component.tagSelect.all': 'تحديد الكل',
+  'component.PPTable.pageTotal': 'Total {total} items. Showing {show} items per page.',
+  'component.PPTable.prev': 'السابق',
+  'component.PPTable.next': 'التالي',
+};
---
+++ src/locales/ar-EG/pwa.ts
@@ -0,0 +1,6 @@
+export default {
+  'app.pwa.offline': 'أنت غير متصل حاليًا',
+  'app.pwa.serviceworker.updated': 'محتوى جديد متاح',
+  'app.pwa.serviceworker.updated.hint': 'الرجاء الضغط على زر" تحديث "لإعادة تحميل الصفحة الحالية',
+  'app.pwa.serviceworker.updated.ok': 'تحديث',
+};
