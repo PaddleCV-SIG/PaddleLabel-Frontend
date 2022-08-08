91e89a8b6c3889be9b4bc41e15b51e133769f91d
Lin Han <me@linhan.email>
2022-08-07 21:46:45 -0400

    feat(cypress): readme

--- cypress/support/config.ts
+++ cypress/support/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  sampleBaseDir: '/home/lin/Desktop/sample', // should not have ending /
+  sampleBaseDir: '/path/to/sample/folder', // should not have ending /, don't forget to \\ on windows
 };
 
 export const runId = new Date().getTime().toString().slice(5, -3);
