b3f5b32c65105709e9052b36e78deb73a70ffd08
Lin Han <me@linhan.email>
2022-11-09 01:23:02 -0500

    chore(intl): add translation

--- src/locales/zh-CN/component.ts
+++ src/locales/zh-CN/component.ts
@@ -19,7 +19,7 @@ export default {
   'component.PPCreater.titleContent': '点此查看有关项目文件结构更多细节',
   'component.PPCreater.folderStructureSample': '数据集文件结构示例',
   'component.PPCreater.folderStructureSampleDetail':
-    '如果希望导入数据集中的标注信息，请参考如上文件结构组织文件夹下的文件',
+    '如果希望导入数据集中的标注信息，请参考如上文件结构组织文件夹下的文件。点击上方文件可查看文件内容示例。',
 
   'component.PPAddLabelModal.selectColor': '选择色彩',
   'component.PPAddLabelModal.addLabel': '创建标签',
