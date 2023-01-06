d1e0dae685b2ddb0bbde0f4af9b30755ce8b428f
Lin Han <me@linhan.email>
2023-01-05 21:32:57 -0500

    feat(cypress): speedup

--- cypress/support/config.ts
+++ cypress/support/config.ts
@@ -3,6 +3,8 @@ export const config = {
   // sampleBaseDir: '~/.paddlelabel/sample/fruit', // should not have ending /, don't forget to \\ on windows
   catgInfo: {
     // semanticSegmentation: { coco: 0 },
+    // detection: { voc: 0 },
+
     classification: { singleClass: 0, multiClass: 0 },
     detection: { coco: 0, voc: 0, yolo: 0 },
     semanticSegmentation: { mask: 0, coco: 0, eiseg: 0 },
