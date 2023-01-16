832d56adfb0dc3b93b87cd38b0cecd014d9952bc
Lin Han <me@linhan.email>
2023-01-16 06:46:29 -0500

    fix(cypress): order tests

--- cypress/order_tests.py
+++ cypress/order_tests.py
@@ -10,11 +10,11 @@ print("screenshots:", [t.name for t in failed_tests])
 failed_ids = [t.name.split("_")[0] for t in failed_tests]
 
 tests = (HERE / "e2e").glob("*.cy.ts")
+tests = list(tests)
+print("all tests:", tests)
+
 all_ids = [t.name.split("_")[0] for t in tests]
 tests = {t.name.split("_")[0]: str(t.relative_to(HERE)) for t in tests}
-
-print(all_ids)
-print(tests)
 
 ordered = [tests[idx] for idx in failed_ids] + [tests[idx] for idx in all_ids if idx not in failed_ids]
 order = ",".join(ordered)
