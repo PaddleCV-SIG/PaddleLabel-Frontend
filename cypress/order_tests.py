671952be39202d60b797f4f71f0fe1f0b0ce06e8
Lin Han <me@linhan.email>
2023-01-16 06:58:24 -0500

    fix(cypress): order test

--- cypress/order_tests.py
+++ cypress/order_tests.py
@@ -3,15 +3,15 @@ from pathlib import Path
 HERE = Path(__file__).parent.absolute()
 
 
-print(list((HERE / ".." / "..").glob("*")))
+# print(list((HERE / ".." / "..").glob("*")))
 
 failed_tests = (HERE / ".." / ".." / "screenshots").glob("*")
-print("screenshots:", [t.name for t in failed_tests])
+# print("screenshots:", [t.name for t in failed_tests])
 failed_ids = [t.name.split("_")[0] for t in failed_tests]
 
 tests = (HERE / "e2e").glob("*.cy.ts")
 tests = list(tests)
-print("all tests:", tests)
+# print("all tests:", tests)
 
 all_ids = [t.name.split("_")[0] for t in tests]
 tests = {t.name.split("_")[0]: str(t.relative_to(HERE)) for t in tests}
