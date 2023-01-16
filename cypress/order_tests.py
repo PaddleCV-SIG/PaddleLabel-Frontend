8547dee4fa43e72838c5bad48cb40782736fd31c
Lin Han <me@linhan.email>
2023-01-16 05:47:16 -0500

    fix(cypress): order test

--- cypress/order_tests.py
+++ cypress/order_tests.py
@@ -5,7 +5,7 @@ HERE = Path(__file__).parent.absolute()
 print(list((HERE / ".." / "..").glob("*")))
 
 failed_tests = (HERE / ".." / ".." / "screenshots").glob("*")
-print(list(failed_tests))
+print("screenshots", list(failed_tests))
 
 all_tests = (HERE / "e2e").glob("*.cy.ts")
-print(list(all_tests))
+all_tests = [t.name.split("_") for t in all_tests]
